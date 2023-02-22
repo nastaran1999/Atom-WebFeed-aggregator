d52545200c1d32ec1b13946e1b289030cd4f8b57
nastaran1999 <nastaran_78_ab@yahoo.com>
2023-02-22 16:45:23 +0330

    @extractus/feed-extractor library replaced with xml2js.

--- src/App.js
+++ src/App.js
@@ -1,78 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import { parseString } from 'xml2js';
-
+// import { parseString } from 'xml2js';
+import { extract } from '@extractus/feed-extractor'
 
 function App() {
   const [feedUrl, setFeedUrl] = useState('');
   const [feedData, setFeedData] = useState(null);
   const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // fetch and parse the feed here
-    axios({
-      url: 'http://127.0.0.1:8080/' + feedUrl,
-      method: 'get',
-    })
-    .then(response => {
-      const xml = response.data;
-      console.log(response.data);
-      parseString(xml, (error, result) => {
-        if (error) {
-          setError(error);
-        } else {
-          console.log(result.feed.entry)
-          console.log(Object.values(result.feed.entry[0].content[0])[0]);
-          let items =  result.feed.entry.map( item => {
-            // title
-            let title = ''
-            if(Array.isArray(item.title)){
-              if(typeof item.title[0] === "object"){
-                title = Object.values(item.title[0])[0]
-              }else{
-                title = item.title[0]
-              }
-            }
-            // update
-            let update = ''
-            if(Array.isArray(item.updated)){
-              update = new Date(item.updated[0]).toLocaleString()
-            }
-            // published
-            let published = ''
-            if(Array.isArray(item.published)){
-              published = new Date(item.published[0]).toLocaleString()
-            }
-            // id
-            let id = ''
-            if(Array.isArray(item.id)){
-              id = item.id[0]
-            }
-            // // content
-            // let content = ''
-            // if(Array.isArray(item.content)){
-            //   if(typeof item.content[0] === "object"){
-            //     content = Object.values(item.content[0])[0]
-            //     console.log(content.toString())
-            //   }
-            // }
-            return {
-              title: title,
-              update: update,
-              published: published,
-              id: id
-              // content: content
-            }
-          } )
-          setFeedData(items);
-        }
-      });
-    })
-    .catch(error => {
-      setError(error);
-    });
+    const result = await extract('http://127.0.0.1:8080/' + feedUrl)
+    setFeedData(result.entries);
   }
 
   return (
@@ -86,10 +27,10 @@ function App() {
         {feedData && feedData.map((item, index) => (
           <li key={index}>
             <div className="title">{item.title}</div>
-            <div className="date">published: {item.published === '' ? 'unknown' : item.published}</div>
-            <div className="date">updated: {item.update}</div>
-            <br />
-            <div className="content">{item.id}</div>
+            <div className="date">{item.published}</div>
+            <div className="content">id: {item.id}</div>
+            <a className="content" href={item.link}>{item.link}</a>
+            <div className="content">description: {item.description}</div>
           </li>
         ))}
       </ul>
