c0bf7a3e4be9a12fbea0b06415d01f6e43bccd8d
nastaran1999 <nastaran_78_ab@yahoo.com>
2023-02-28 12:10:53 +0330

    author data + unloaded image handling added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 import axios from 'axios';
 // import { parseString } from 'xml2js';
 import { extract } from '@extractus/feed-extractor'
@@ -12,9 +12,25 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     // fetch and parse the feed here
-    const result = await extract('http://127.0.0.1:8080/' + feedUrl)
-    setFeedData(result.entries);
+    const result = await extract('http://127.0.0.1:8080/' + feedUrl, {
+      normalization: false
+    })
+    console.log(result)
+    console.log(result.entry)
+    setFeedData(result);
+
   }
+
+  // handling uloaded images with relative paths
+  useEffect(() => {
+    let imgElement = document.getElementsByTagName('img')[0]
+    if(imgElement){
+      if(!imgElement.complete && imgElement.naturalHeight == 0){
+        imgElement.src = feedData.link + feedData.logo
+      }
+    }
+  }, [feedData])
+  
 
   return (
     <div className="App">
@@ -23,14 +39,34 @@ function App() {
         <button className='button' type="submit">Subscribe</button>
       </form>
       {error && <div className="error">{error.message}</div>}
+      {
+        feedData ? 
+        <div className='info-container'>
+          <div className='info-upper-container'>
+            <div className='info-upper-left'>
+              <p>author: {feedData.author.name}</p>
+              <p>id: {feedData.id}</p>
+              <p>link: {feedData.link}</p>
+              <p>title: {feedData.title}</p>
+            </div>
+            <img src={feedData.logo} />
+          </div>
+          <p>subtitle: {feedData.subtitle}</p>
+          <p>rights: {feedData.rights}</p>
+          <p>updated: {feedData.updated}</p>
+        </div>
+        :
+        null
+      }
+      
       <ul>
-        {feedData && feedData.map((item, index) => (
+        {feedData && feedData.entry.map((item, index) => (
           <li key={index}>
             <div className="title">{item.title}</div>
             <div className="date">{item.published}</div>
             <div className="content">id: {item.id}</div>
             <a className="content" href={item.link}>{item.link}</a>
-            <div className="content">description: {item.description}</div>
+            <div className="content">description: {item.content}</div>
           </li>
         ))}
       </ul>
