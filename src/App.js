d8cd614e5168070951bce577830a4d339e9c67eb
nastaran1999 <nastaran_78_ab@yahoo.com>
2023-03-09 18:01:22 +0330

    pagination without styles added.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { extract } from '@extractus/feed-extractor'
 
+const PAGE_SIZE = 10; // number of items to show per page
+const PAGE_RANGE = 5; // number of page buttons to show in the range
+
 function App() {
   const [feedUrl, setFeedUrl] = useState('');
   const [feedData, setFeedData] = useState([]);
   const [error, setError] = useState(null);
   localStorage.setItem("savedFeeds", JSON.stringify([]));
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -43,6 +47,43 @@ function App() {
     concatFeeds = sortAccordingToDate(concatFeeds);
     setFeedData(concatFeeds)
   }
+
+  // calculate the total number of pages
+  const totalPages = Math.ceil(feedData.length / PAGE_SIZE);
+
+  // calculate the range of page numbers to display
+  let pageRangeStart = Math.max(1, currentPage - PAGE_RANGE);
+  let pageRangeEnd = Math.min(totalPages, currentPage + PAGE_RANGE);
+  if (pageRangeEnd - pageRangeStart < PAGE_RANGE * 2) {
+    // adjust the range if it's too small
+    if (pageRangeStart === 1) {
+      pageRangeEnd = Math.min(totalPages, pageRangeStart + PAGE_RANGE * 2);
+    } else {
+      pageRangeStart = Math.max(1, pageRangeEnd - PAGE_RANGE * 2);
+    }
+  }
+
+  // create an array of page numbers to display
+  const pageNumbers = [];
+  if (pageRangeStart > 1) {
+    pageNumbers.push(1);
+    if (pageRangeStart > 2) {
+      pageNumbers.push("...");
+    }
+  }
+  for (let i = pageRangeStart; i <= pageRangeEnd; i++) {
+    pageNumbers.push(i);
+  }
+  if (pageRangeEnd < totalPages) {
+    if (pageRangeEnd < totalPages - 1) {
+      pageNumbers.push("...");
+    }
+    pageNumbers.push(totalPages);
+  }
+
+  // calculate the start and end index of the current page
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
 
   // setting identical field names for all types of feeds
   const changeFieldNameOfArray = (old_key, new_key, array) => {
@@ -119,37 +160,59 @@ function App() {
         <button className='button' type="submit">Subscribe</button>
       </form>
       {error && <div className="error">{error.message}</div>}
-            <div className='info-container'>
-              <ul>
-              {
-                feedData.length && feedData.map((item, index) => (
-                  <li key={index}>
-                    <div className="author_container">
-                      <img className='author_img' src={item.logo} />
-                      <div className='author_info_wrapper'>
-                        <p className='author_info_title'>{item.title}</p>
-                        <p className='author_info_name'>{item.author} - <a href={item.authorLink}>{item.authorLink}</a></p>
-                      </div>
-                    </div>
-                    <div className='content_container'>
-                      <div className="date">{item.published ? item.published : 'unavailable'}</div>
-                      <a className="date" href={item.link}>{item.link}</a>
-                      <div className="content">{contentHandler(item)}</div>
-                    </div>
-                    <button className="add_btn" 
-                      id={index + '_btn'}
-                      onClick={() => {
-                        let successfullyAdded = addFeed(item)
-                        if(successfullyAdded){
-                          document.getElementById(index + '_btn').disabled = true
-                        }
-                      }}
-                      >Add</button>
-                  </li>
-                ))
-              }
-              </ul>
-            </div>
+      <div className='info-container'>
+        <ul>
+        {
+          feedData.length && feedData.slice(startIndex, endIndex).map((item, index) => (
+            <li key={index}>
+              <div className="author_container">
+                <img className='author_img' src={item.logo} />
+                <div className='author_info_wrapper'>
+                  <p className='author_info_title'>{item.title}</p>
+                  <p className='author_info_name'>{item.author} - <a href={item.authorLink}>{item.authorLink}</a></p>
+                </div>
+              </div>
+              <div className='content_container'>
+                <div className="date">{item.published ? item.published : 'unavailable'}</div>
+                <a className="date" href={item.link}>{item.link}</a>
+                <div className="content">{contentHandler(item)}</div>
+              </div>
+              <button className="add_btn" 
+                id={index + '_btn'}
+                onClick={() => {
+                  let successfullyAdded = addFeed(item)
+                  if(successfullyAdded){
+                    document.getElementById(index + '_btn').disabled = true
+                  }
+                }}
+                >Add</button>
+            </li>
+          ))
+        }
+        </ul>
+        <div>
+          <button onClick={() => setCurrentPage(1)} disabled={currentPage === 1} >
+            First
+          </button>
+          <button onClick={() => setCurrentPage(prevPage => prevPage - 1)} disabled={currentPage === 1}>
+            Previous
+          </button>
+          {pageNumbers.map(pageNumber => (
+            <button
+              key={pageNumber}
+              onClick={() => setCurrentPage(pageNumber)}
+              disabled={currentPage === pageNumber}>
+              {pageNumber}
+            </button>
+          ))}
+          <button onClick={() => setCurrentPage(prevPage => prevPage + 1)} disabled={currentPage === totalPages}>
+            Next
+          </button>
+          <button onClick={() => setCurrentPage(totalPages)} disabled={currentPage === totalPages}>
+            Last
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
